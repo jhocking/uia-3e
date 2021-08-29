10cbf324febe45e2931e7c6afc30878b0da23604
Joseph Hocking <jhocking@newarteest.com>
2021-08-29 15:06:13 -0500

    chapter 5 tmpro

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+	[SerializeField] GameObject cardBack;
+	[SerializeField] SceneController controller;
+
+	private int _id;
+	public int Id {
+		get {return _id;}
+	}
+
+	public void SetCard(int id, Sprite image) {
+		_id = id;
+		GetComponent<SpriteRenderer>().sprite = image;
+	}
+
+	public void OnMouseDown() {
+		if (cardBack.activeSelf && controller.canReveal) {
+			cardBack.SetActive(false);
+			controller.CardRevealed(this);
+		}
+	}
+
+	public void Unreveal() {
+		cardBack.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+	[SerializeField] GameObject targetObject;
+	[SerializeField] string targetMessage;
+	public Color highlightColor = Color.cyan;
+
+	public void OnMouseEnter() {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		if (sprite != null) {
+			sprite.color = highlightColor;
+		}
+	}
+	public void OnMouseExit() {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		if (sprite != null) {
+			sprite.color = Color.white;
+		}
+	}
+
+	public void OnMouseDown() {
+		transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+	}
+	public void OnMouseUp() {
+		transform.localScale = Vector3.one;
+		if (targetObject != null) {
+			targetObject.SendMessage(targetMessage);
+		}
+	}
+}
